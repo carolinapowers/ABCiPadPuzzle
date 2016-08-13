48b041d6276f45eb887f38fd2a0154fc3a910caf
cpowers <cpowers@deloitte.com>
2016-08-12 21:00:53 -0400

    replaced images and added audio

--- www/app/main/main.controller.js
+++ www/app/main/main.controller.js
@@ -5,6 +5,7 @@
 
     function MainCtrl($scope) {
         var vm = this;
+        vm.matched = false;
 
         var myAudio = document.createElement('audio');
         myAudio.controls = true;
@@ -21,7 +22,7 @@
         $scope.onDropComplete = function (targetLetter, data, evt) {
             var vm = this;
             if (targetLetter === data.id) {
-                document.getElementById(targetLetter + "-target").appendChild(evt.element[0]);
+                //document.getElementById(targetLetter + "-target").appendChild(evt.element[0]);
                 myAudio.src = "../../assets/" +targetLetter+ ".mp3" ;
                 myAudio.play();
 
@@ -32,6 +33,7 @@
                         vm.vm.verticalLetters.splice(i, 1);
                     }
                 }
+                vm.matched = true;
             } else {
                 return;
             }
@@ -39,107 +41,107 @@
 
 
         vm.verticalLetters = [{
-                src: "assets/images/A.png",
+                src: "assets/images/a_color.png",
                 id: "a1",
             },
             {
-                src: "assets/images/B.png",
+                src: "assets/images/b_color.png",
                 id: "b1"
                       },
             {
-                src: "assets/images/C.png",
+                src: "assets/images/c_color.png",
                 id: "c1"
             },
             {
-                src: "assets/images/D.png",
+                src: "assets/images/d_color.png",
                 id: "d1"
             },
             {
-                src: "assets/images/E.png",
+                src: "assets/images/e_color.png",
                 id: "e1"
             },
             {
-                src: "assets/images/F.png",
+                src: "assets/images/f_color.png",
                 id: "f1"
             },
             {
-                src: "assets/images/G.png",
+                src: "assets/images/g_color.png",
                 id: "g1"
             },
             {
-                src: "assets/images/H.png",
+                src: "assets/images/h_color.png",
                 id: "h1"
             },
             {
-                src: "assets/images/I.png",
+                src: "assets/images/i_color.png",
                 id: "i1"
             },
             {
-                src: "assets/images/J.png",
+                src: "assets/images/j_color.png",
                 id: "j1"
             },
             {
-                src: "assets/images/K.png",
+                src: "assets/images/k_color.png",
                 id: "k1"
             },
             {
-                src: "assets/images/L.png",
+                src: "assets/images/l_color.png",
                 id: "l1"
             },
             {
-                src: "assets/images/M.png",
+                src: "assets/images/m_color.png",
                 id: "m1"
             },
             {
-                src: "assets/images/N.png",
+                src: "assets/images/n_color.png",
                 id: "n1"
             },
             {
-                src: "assets/images/O.png",
+                src: "assets/images/o_color.png",
                 id: "o1"
             },
             {
-                src: "assets/images/P.png",
+                src: "assets/images/p_color.png",
                 id: "p1"
             },
             {
-                src: "assets/images/Q.png",
+                src: "assets/images/q_color.png",
                 id: "q1"
             },
             {
-                src: "assets/images/R.png",
+                src: "assets/images/r_color.png",
                 id: "r1"
             },
             {
-                src: "assets/images/S.png",
+                src: "assets/images/s_color.png",
                 id: "s1"
             },
             {
-                src: "assets/images/T.png",
+                src: "assets/images/t_color.png",
                 id: "t1"
             },
             {
-                src: "assets/images/U.png",
+                src: "assets/images/u_color.png",
                 id: "u1"
             },
             {
-                src: "assets/images/V.png",
+                src: "assets/images/v_color.png",
                 id: "v1"
             },
             {
-                src: "assets/images/W.png",
+                src: "assets/images/w_color.png",
                 id: "w1"
             },
             {
-                src: "assets/images/X.png",
+                src: "assets/images/x_color.png",
                 id: "x1"
             },
             {
-                src: "assets/images/Y.png",
+                src: "assets/images/y_color.png",
                 id: "y1"
             },
             {
-                src: "assets/images/Z.png",
+                src: "assets/images/z_color.png",
                 id: "z1"
             }
     ];
